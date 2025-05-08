c5446bbe00bfe78b34985ea6fee61dd1b8190947
Igor Minar <iminar@cloudflare.com>
2025-05-08 10:42:55 -0700

    fix support for direct use of $localize in react
    without an interator patch, the string renders as an array causing hydration errors

--- jsx$localize/src/react/localize-override.ts
+++ jsx$localize/src/react/localize-override.ts
@@ -11,13 +11,35 @@ import {ɵ$localize as $localizeOriginal} from '@angular/localize';
 // Extend the String class to pass instanceof check and inherit all String methods
 class LocalizedString extends String {
   constructor(strings: TemplateStringsArray, ...values: unknown[]) {
-    super('asdf');
+    super();
     
     const localizedStringFn = () => $localizeOriginal(strings, ...values);
 
     // Override toString and valueOf to support string concatenation and stringification
     this.toString = localizedStringFn;
     this.valueOf = localizedStringFn;
+  }
+
+  // Implementing the iterator protocol on the class because React otherwise considers this object to be an iterable array and renders the string one character at a time.
+  // @ts-ignore
+  [Symbol.iterator] = () => {
+    // Use the class instance in the iterator
+    return {
+      // Using the class context to track state
+      done: false,
+      toString: this.toString,
+      
+      next() {
+        if (this.done) {
+          return { value: undefined, done: true };
+        } else {
+          // Mark as read so future iterations return nothing
+          this.done = true;
+          // Return the translated string as a single value and only value from this iterator
+          return { value: this.toString(), done: false };
+        }
+      }
+    };
   }
 }
 
@@ -32,3 +54,10 @@ globalThis.$localize = function $localizeDeferred(strings: TemplateStringsArray,
     }
   });
 }
+
+// monkey patch the original $localize so that it can find the translations loaded via `loadTranslations` during runtime localization (SSR)
+Object.defineProperty($localizeOriginal, 'translate', {
+  get() {
+    return $localize.translate;
+  }
+});
